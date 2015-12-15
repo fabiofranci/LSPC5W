470b3aac38b8da95f8183517699781be12d2d1e9
fabio franci <fabio.franci@gmail.com>
2015-12-15 16:28:48 +0100

    Vai

--- www/js/index.js
+++ www/js/index.js
@@ -1835,7 +1835,7 @@ function onDeviceReady() {
     var success='';
     var error='';
 
-    //db = window.openDatabase("LASAETTAPC5DB", "1.0.1", "Database La Saetta PC5", 200000);
+//    db = window.openDatabase("LASAETTAPC5DB", "1.0.1", "Database La Saetta PC5", 200000);
     db = window.sqlitePlugin.openDatabase("LASAETTAPC5DB", "1.0.1", "Database La Saetta PC5", 200000);
     try {
         db.transaction(function (tx) {
